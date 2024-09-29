105c3d4dc07ab5e4ac00950254f85e185f29e256
Kirill <larkin.kirill@mail.ru>
2024-09-30 02:14:34 +0300

    1+1+1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,6 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println(1+1+1);
-        System.out.println(2+2+2);
-        System.out.println(3+3+3);
+
     }
 }
