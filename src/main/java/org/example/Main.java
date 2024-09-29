15ac06bd92880cfe263905e0373c7e7c08d3fc81
Kirill <larkin.kirill@mail.ru>
2024-09-30 01:51:03 +0300

    good

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,6 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("branch2");
-        System.out.println("Commit");
+        System.out.println(2+2+2);
     }
 }
