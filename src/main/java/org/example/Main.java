34a217f9e28c5a7e50b4fa717bf30bd5edc40851
Kirill <larkin.kirill@mail.ru>
2024-09-30 02:55:37 +0300

    1+1+1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,16 @@
 package org.example;
 
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        String number = "12345678";
+        String delimiter = ",";
+        System.out.println(number.replaceAll("(\\d)(?=(\\d{3})+(?!\\d))", "$1,"));
+
+
 
     }
+
 }
