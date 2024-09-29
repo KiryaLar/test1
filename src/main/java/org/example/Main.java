d3610c14db588f11964a2fc69f9127ee178cc905
Kirill <larkin.kirill@mail.ru>
2024-09-30 02:57:52 +0300

    scanner

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,14 @@
 package org.example;
 
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String number = "12345678";
-        String delimiter = ",";
+        Scanner scanner = new Scanner(System.in);
+        String number = String.valueOf(scanner.nextInt());
         System.out.println(number.replaceAll("(\\d)(?=(\\d{3})+(?!\\d))", "$1,"));
 
 
