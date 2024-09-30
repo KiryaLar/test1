0667ccd892331ce8df7b5c7d8129d559ca4b453a
Kirill <larkin.kirill@mail.ru>
2024-09-30 19:46:50 +0300

    new array

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,10 +7,19 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        for (int x : array) {
+        int[] array1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int x : array1) {
             System.out.println(x);
         }
+        int[] array2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int x : array2) {
+            System.out.println(x);
+        }
+        int[] array3 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int x : array3) {
+            System.out.println(x);
+        }
+
 
     }
 
